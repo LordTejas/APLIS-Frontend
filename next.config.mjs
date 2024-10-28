e0d8f82dee2a34420c44549c6cdc970e0b28d2d7
LordTejas <tejas5225@gmail.com>
2024-10-28 23:49:16 +0530

    Fix: Next js Build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   env: {
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     JWT_SECRET: process.env.JWT_SECRET,
+    SESSION_SECRET: process.env.SESSION_SECRET,
     DATABASE_URL: process.env.DATABASE_URL
   }
 };
